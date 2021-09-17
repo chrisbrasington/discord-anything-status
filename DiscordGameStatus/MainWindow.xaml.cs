46e1fc472e559846a8d05a34673a1e1404df622a
Christopher Brasington <cbrasington@dmns.org>
2021-09-17 13:11:14 -0600

    initial functional

---
+++ DiscordGameStatus/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace DiscordGameStatus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LaunchWindow : Window
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        private string _gameListName = "GameList";
+        private string _inputTextName = "Input";
+
+        /// <summary>
+        /// game list
+        /// TODO: make editable
+        /// </summary>
+        private List<string> _gamelist = new List<string>();
+
+
+        /// <summary>
+        /// Launch Window
+        /// </summary>
+        public LaunchWindow()
+        {
+            Setup();
+        }
+
+        /// <summary>
+        /// Initial Setup
+        /// </summary>
+        public void Setup()
+        {
+            //var currentProc = System.Diagnostics.Process.GetCurrentProcess();
+            //string name = currentProc.ProcessName;
+
+            InitializeComponent();
+            SetupGameList();
+        }
+
+        /// <summary>
+        /// Setup game list dropdown
+        /// </summary>
+        private void SetupGameList()
+        {
+            _gamelist.Add("Monster Hunter Rise");
+            _gamelist.Add("Splatoon 2");
+
+            var comboBox = (ComboBox)this.FindName(_gameListName);
+
+            foreach (string game in _gamelist)
+            {
+                comboBox.Items.Add(game);
+            }
+
+            comboBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// click GO button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // get active game to set
+            string _activeGame = "";
+            var inputText = (TextBox)this.FindName(_inputTextName);
+
+            // use text box
+            if (!string.IsNullOrEmpty(inputText.Text))
+            {
+                _activeGame = inputText.Text;
+            }
+            // use dropdown (text box is empty)
+            else
+            {
+                var comboBox = (ComboBox)this.FindName(_gameListName);
+                _activeGame = comboBox.SelectedItem.ToString();
+            }
+
+            // source running executable
+            string source = $"{Environment.CurrentDirectory}\\DiscordGameStatus.exe";
+            // destination running executable
+            string destination = $"{Environment.CurrentDirectory}\\{_activeGame}.exe";
+
+
+            // create new executible (see README.md)
+            File.Copy(source, destination, true);
+
+            // start process
+            Process p = Process.Start(destination);
+            SpinWait.SpinUntil(() => p.MainWindowHandle != IntPtr.Zero);
+            SetWindowText(p.MainWindowHandle, _activeGame);
+
+            // terminiate this process
+            this.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # discord-anything-status
+
+This is real quickly hacked together.
+
+Program will launch as "Discrod Game Status". Do NOT add this to discord's activity status.
+First, set the active game by either the dropdown or the input textbox and clicking GO.
+
+![Alt text](Images/program1.png "")
+
+![Alt text](Images/program2.png "")
+
+From here you can add the game manually in discord's activity status. I'd love to get this to auto-detect, but am unsure the mechanism (authorship?) atm. Unforunately that means adding it per game title.
+
+![Alt text](Images/discord1.png "")
+
+![Alt text](Images/discord2.png "")
+
+![Alt text](Images/discord3.png "")
+
+How this works... discord seems to associate by the process name and exe name. It's not enough to just rename the process or discord will only think it's a single game. What this program does is duplicate its executible by game name, kill the active process, and relaunch a new one. 
+
+![Alt text](Images/task1.png "")
+
+![Alt text](Images/executables1.png "")
